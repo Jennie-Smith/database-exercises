4a1f272649bed99f15cedab8f0ae3437b787cef6
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-01 14:27:15 -0600

    update

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,10 @@
 USE codeup_test_db;
+    CREATE TABLE albums (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        artist_name VARCHAR(50),
+        name_record VARCHAR(150),
+        release_date INT,
+        sales FLOAT,
+        genre VARCHAR(50),
+        PRIMARY KEY (id)
+    )
---
+++ select_exercises.sql
@@ -0,0 +1,23 @@
+USE codeup_test_db;
+-- INSERT INTO quotes (author_first_name, author_last_name, content)
+-- VALUES ('Douglas', 'Adams', 'I love deadlines. I love the whooshing noise they make as they go by.');
+-- (`id`, `artist`, `album_name`, `release_date`, `sales`, `genre`)
+--     The name of all albums by Pink Floyd.
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+-- year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date FROM albums WHERE artist = 'The Beatles';
+
+-- The genre for Nevermind
+SELECT * FROM albums WHERE genre = 'Nevermind';
+-- get no rows
+
+-- Which albums were released in the 1990s
+SELECT * FROM albums WHERE release_date BETWEEN '1990' AND '2000';
+
+-- Which albums had less than 20 million certified sales
+SELECT * FROM albums WHERE sales < '20.000000';
+
+-- All the albums with a genre of "Rock".
+-- Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT * FROM albums WHERE genre = 'Rock';
