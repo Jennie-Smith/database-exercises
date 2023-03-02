0009006dbe5ada2affd525ecdc2d486c1b4f9978
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-02 13:50:14 -0600

    update '

---
+++ limit_exercises.sql
@@ -0,0 +1,21 @@
+# example:
+# SELECT emp_no, first_name, last_name
+# FROM employees
+# WHERE first_name Like 'M%'
+# ORDER BY emp_no
+# LIMIT 10 OFFSET 50;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary ASC
+LIMIT 5;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary ASC
+LIMIT 5 OFFSET 10;
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,6 +3,11 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name ASC, first_name DESC;
 
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
 SELECT emp_no, last_name
 FROM employees
 WHERE last_name LIKE 'e%'
