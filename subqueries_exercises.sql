5d02aa909b2df8afa24a6c90ff71d068d6ca2e61
Jennie Smith <Jsmith9spiral@gmail.com>
2023-03-03 16:15:50 -0600

    update

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -25,20 +25,15 @@ where t.emp_no IN (
 #               on dm.dept_no = d.dept_no
 # WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
 
-SELECT e.first_name, e.last_name,
+SELECT e.first_name, e.last_name
 FROM employees as e
 #          join dept_manager dm
 #               on e.emp_no = dm.emp_no
 #          join departments d
 #               on dm.dept_no = d.dept_no
 # WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
-where e.emp_no IN (
+where e.gender = 'F' AND e.emp_no IN (
     select dm.emp_no
     from dept_manager as dm
-    where dm.to_date = '9999-01-01'
-    ) IN (
-select d.dept_no
-from departments d
-where e.gender = 'F'
-    );
+    where dm.to_date = '9999-01-01');
 
